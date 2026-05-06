64a2a680a0c7cd663a59ec30fad85e4b66446424
Hcom <72270756+74dine@users.noreply.github.com>
2026-05-06 17:09:21 +0530

    add solution for 'implement queue using stacks'

---
+++ src/implement_queue_using_stacks.rs
@@ -0,0 +1,91 @@
+﻿struct MyQueue {
+    a: Vec<i32>,
+}
+
+#[allow(dead_code)]
+impl MyQueue {
+    fn new() -> Self {
+        MyQueue { a: Vec::new() }
+    }
+
+    fn push(&mut self, x: i32) {
+        let mut temp = Vec::with_capacity(self.a.len());
+
+        for _ in 0..self.a.len() {
+            temp.push(self.a.pop().unwrap());
+        }
+
+        self.a.push(x);
+
+        for _ in 0..temp.len() {
+            self.a.push(temp.pop().unwrap());
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.a.pop().unwrap()
+    }
+
+    fn peek(&self) -> i32 {
+        *self.a.last().unwrap()
+    }
+
+    fn empty(&self) -> bool {
+        self.a.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod implement_queue_using_stacks_tests {
+    use super::MyQueue;
+
+    #[test]
+    fn lc_case_1() {
+        let mut queue = MyQueue::new();
+
+        queue.push(1);
+        queue.push(2);
+
+        assert_eq!(queue.peek(), 1);
+
+        assert_eq!(queue.pop(), 1);
+
+        assert_eq!(queue.empty(), false);
+    }
+
+    #[test]
+    fn does_initialize() {
+        MyQueue::new();
+    }
+
+    #[test]
+    fn does_push() {
+        let mut queue = MyQueue::new();
+
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+    }
+
+    #[test]
+    fn does_pop() {
+        let mut queue = MyQueue::new();
+
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+
+        assert_eq!(queue.pop(), 1);
+    }
+
+    #[test]
+    fn does_validate_empty() {
+        let mut queue = MyQueue::new();
+
+        assert_eq!(queue.empty(), true);
+
+        queue.push(1);
+
+        assert_eq!(queue.empty(), false);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod test_runner;
 mod best_time_to_buy_and_sell_stock_ii;
 mod implement_stack_using_queues;
+mod implement_queue_using_stacks;
 
 #[allow(unreachable_code)]
 #[allow(dead_code)]
