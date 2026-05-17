9d3dd9a59969a539df4d638dab0156f905b5f6a9
Hcom <72270756+74dine@users.noreply.github.com>
2026-05-17 07:12:21 +0530

    feat(next greater element I): "optimal" O(n+m) solution

--- src/next_greater_element_i.rs
+++ src/next_greater_element_i.rs
@@ -1,16 +1,26 @@
-﻿#[allow(dead_code)]
-pub fn next_greater_element(nums: Vec<i32>, search_arr: Vec<i32>) -> Vec<i32> {
-    nums.iter()
-        .map(|x| {
-            let Some(idx) = search_arr.iter().position(|&y| y == *x) else {
-                return -1;
-            };
+﻿use std::collections::HashMap;
 
-            *search_arr[idx + 1..]
-                .iter()
-                .find(|&&y| y > *x)
-                .unwrap_or(&-1)
-        })
+#[allow(dead_code)]
+pub fn next_greater_element(nums: Vec<i32>, search_arr: Vec<i32>) -> Vec<i32> {
+    let mut queue = vec![];
+    let mut known = HashMap::new();
+
+    for &num in &search_arr {
+        while let Some(&next) = queue.last() {
+            if next < num {
+                known.insert(next, num);
+                queue.pop();
+                continue;
+            }
+
+            break;
+        }
+
+        queue.push(num);
+    }
+
+    nums.iter()
+        .map(|x| *known.get(&x).unwrap_or(&-1))
         .collect()
 }
 
@@ -32,5 +42,13 @@ mod next_greater_element_i_tests {
             vec![3, -1],
             next_greater_element(vec![2, 4], vec![1, 2, 3, 4])
         );
+    }
+
+    #[test]
+    fn lc_case_3() {
+        assert_eq!(
+            vec![-1, 3, 3],
+            next_greater_element(vec![3, 1, 2], vec![2, 1, 3])
+        )
     }
 }
